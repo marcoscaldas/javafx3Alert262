4bc01d1d0f1dee778651434f4c080883fdda2544
marcoscaldas <donarumo@hotmail.com>
2020-09-10 17:15:12 -0300

    Alerts

---
+++ src/gui/ViewController.java
@@ -0,0 +1,19 @@
+package gui;
+
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
+
+public class ViewController {
+	
+	@FXML
+	private Button btTeste;
+	
+	@FXML
+	public void onBtTestAction () {
+		Alerts.showAlert("Alert title", null, "Hello", AlertType.INFORMATION); // no lugar de nulo pode colocar o alert header cabeçalho
+	}
+	
+
+}
